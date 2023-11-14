521a8da5467a437e100adc7f24cc82cb40601446
SalnikovMarkiian <salnikov.markiyan@gmail.com>
2023-11-14 10:30:10 +0200

    minor fix

--- src/main/java/com/example/onlinebookstore/model/Book.java
+++ src/main/java/com/example/onlinebookstore/model/Book.java
@@ -13,6 +13,8 @@ import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Set;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
@@ -45,6 +47,8 @@ public class Book {
     @Column(nullable = false)
     private boolean isDeleted = false;
 
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
     @ManyToMany
     @JoinTable(
             name = "books_categories",

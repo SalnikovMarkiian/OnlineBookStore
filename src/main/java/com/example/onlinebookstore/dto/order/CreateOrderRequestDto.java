e1dfaed3b7049ce8b919de3218cbe0d86b9468ca
SalnikovMarkiian <salnikov.markiyan@gmail.com>
2023-11-15 15:26:15 +0200

    minor fix

--- src/main/java/com/example/onlinebookstore/dto/order/CreateOrderRequestDto.java
+++ src/main/java/com/example/onlinebookstore/dto/order/CreateOrderRequestDto.java
@@ -1,9 +1,9 @@
 package com.example.onlinebookstore.dto.order;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
 public record CreateOrderRequestDto(
-        @NotNull @Length(max = 128)
+        @NotBlank @Length(max = 128)
         String shippingAddress) {
 }
